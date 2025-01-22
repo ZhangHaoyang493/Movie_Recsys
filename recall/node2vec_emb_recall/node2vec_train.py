5b1f4e39bb8fca17970bf694dc05899aa2298a58
张皓阳 <zhanghaoyang@zhanghaoyangdeMacBook-Air.local>
2025-01-22 23:25:08 +0800

    完成了word2vec的训练以及召回，又添加了node2vec

---
+++ recall/node2vec_emb_recall/node2vec_train.py
@@ -0,0 +1,53 @@
+import gensim
+import yaml
+import pickle
+import random
+from tqdm import tqdm
+import os.path as osp
+
+with open('./w2v_conf.yaml', 'r') as f:
+    conf = yaml.safe_load(f)
+
+train_readlist: dict = pickle.load(open(conf['train_readlist_path'], 'rb'))
+
+# 构图
+# 给item的id统统加上user的个数，防止id重复
+graph: dict = {}
+for userid in train_readlist.keys():
+    for item_info in train_readlist[userid]:
+        itemid = item_info[0]
+        itemid = str(int(itemid) + conf['user_num'])
+        if userid not in graph:
+            graph[userid] = []
+        graph[userid].append(itemid)
+        if itemid not in graph:
+            graph[itemid] = []
+        graph[itemid].append(userid)
+
+graph_walk_path = []
+
+
+
+# 构造word2vec的训练样本
+# train_sentence = []
+# for user_id in tqdm(train_readlist.keys()):
+#     item_sequence = train_readlist[user_id]
+#     item_sequence = [x[0] for x in item_sequence]
+#     if len(item_sequence) >= conf['sequence_len']:
+#         for i in range(len(item_sequence) - conf['sequence_len']):
+#             train_sentence.append(item_sequence[i: i + conf['sequence_len']])
+#     else:
+#         ori_item_seq = item_sequence[:]
+#         while len(item_sequence) < conf['sequence_len']:
+#             item_sequence += ori_item_seq
+#         train_sentence.append(item_sequence[:conf['sequence_len']])
+
+model = gensim.models.Word2Vec(
+    train_sentence,
+    vector_size=conf['emb_dim'],
+    window=conf['window_size'],
+    min_count=1,
+    workers=4
+)
+
+model.save(osp.join(conf['save_path'], 'word2vec_model.pth'))
