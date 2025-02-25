a276d69b78d8465714989f2d119702a815197b72
zhanghaoyang04 <zhanghaoyang04@baidu.com>
2025-02-25 16:18:41 +0800

    添加了DIN，待测

--- sort/sortDataLoader.py
+++ sort/sortDataLoader.py
@@ -15,6 +15,8 @@ class SortModelDataLoader(Dataset):
                  movie_info_path: str,
                  user_info_path: str,
                  eval_data_path: str,
+                 history_len:int=5,
+                 kind_len:int=10,
                  type='train'): #, one_hot: bool=False, user_num:int =6040, item_num:int =3952):
         
 
@@ -22,11 +24,25 @@ class SortModelDataLoader(Dataset):
         self.all_data = []
         self.item_info = pickle.load(open(movie_info_path, 'rb'))
         self.user_info = pickle.load(open(user_info_path, 'rb'))
+        self.user_pos_history = {}
+
+        self.history_len = history_len
+        self.kind_len = kind_len
+
+        # 获取用户正向的阅读历史
+        for userid in train_readlist.keys():
+            if userid not in self.user_pos_history.keys():
+                self.user_pos_history[userid] = []
+            for item_info in train_readlist[userid]:
+                if item_info[1] >= 4:
+                    self.user_pos_history[userid].append((item_info[0], item_info[1], item_info[2]))
+
         if type == 'train':
             train_readlist: dict = pickle.load(open(train_readlist_path, 'rb'))
             for userid in train_readlist.keys():
                 for item_info in train_readlist[userid]:
                     self.all_data.append([userid, item_info[0], item_info[1], item_info[2]])
+                    
         elif type == 'test':
             eval_data: dict = pickle.load(open(eval_data_path, 'rb'))
             for userid in eval_data.keys():
@@ -61,9 +77,48 @@ class SortModelDataLoader(Dataset):
     def __len__(self):
         return len(self.all_data)
     
+    # 返回大于当前时间戳的第一个物品的索引
+    def binary_search_history(self, userid, ts_now):
+        readlist = self.user_pos_history[userid]
+        left = 0
+        right = len(readlist) - 1
+        ans_index = 0
+        while left <= right:
+            mid = (left + right) // 2
+            if readlist[mid][2] > ts_now:
+                right = mid - 1
+                ans_index = right + 1
+            elif readlist[mid][2] < ts_now:
+                left = mid + 1
+                ans_index = left
+            elif readlist[mid][2] == ts_now:
+                return mid
+        return ans_index
+        
+    def get_item_kind(self, itemid):
+        item_info = self.item_info[itemid]
+        item_kind = item_info[-1]
+        item_kind = [self.gene_dict[k] for k in item_kind]
+        while len(item_kind) < self.kind_len:
+            item_kind.append(0)
+        return item_kind
+    
+    # 返回用户历史阅读列表
+    def get_user_his_list(self, userid, ts_now):
+        user_his_item_id = []
+        user_his_item_kind = []
+        history_index = self.binary_search_history(userid, ts_now)
+        his_list = self.user_pos_history[userid][:history_index]
+        for his in his_list:
+            user_his_item_id.append(int(his[0]))
+            user_his_item_kind.append(self.get_item_kind())
+        while len(user_his_item_id) < self.history_len:
+            user_his_item_id = [0] + user_his_item_id
+            user_his_item_kind = [[0] * self.kind_len] + user_his_item_kind
+        return user_his_item_id, user_his_item_kind
 
     def __getitem__(self, index):
-        userid, itemid, score, _ = self.all_data[index]
+        userid, itemid, score, ts_now = self.all_data[index]
 
         
         user_info = self.user_info[userid]
@@ -75,10 +130,11 @@ class SortModelDataLoader(Dataset):
         user_age = int(user_info[2])
         user_occupation = int(user_info[3])
 
-        item_kind = item_info[-1]
-        item_kind = [self.gene_dict[k] for k in item_kind]
-        while len(item_kind) < 10:
-            item_kind.append(0)
+        item_kind = self.get_item_kind(itemid)
+
+        his_id, his_kind = self.get_user_his_list(userid, ts_now)
+        
+        
         
         # if not self.one_hot:
         return {
@@ -89,6 +145,8 @@ class SortModelDataLoader(Dataset):
             'user_age': torch.tensor([self.age_dict[user_age]]),
             'user_occupation': torch.tensor([user_occupation]),
             'item_kind': torch.tensor(item_kind),
+            'item_id_his': torch.tensor(his_id),
+            'item_kind_his': torch.tensor(his_kind),
             'label': torch.tensor([1 if score >= 4 else 0])
         }
         # else:
@@ -114,13 +172,15 @@ class SortModelDataLoader(Dataset):
         #     } 
 
 
-def get_sort_dataloader(batch_size: int=1, num_workers:int = 4, type: str='train'):
+def get_sort_dataloader(batch_size: int=1, num_workers:int = 4, type: str='train', his_len=5, kind_len=10):
     dataset = SortModelDataLoader(
         '/Users/zhanghaoyang04/Desktop/Movie_Recsys/cache/train_readlist.pkl',
         '/Users/zhanghaoyang04/Desktop/Movie_Recsys/cache/movie_info.pkl',
         '/Users/zhanghaoyang04/Desktop/Movie_Recsys/cache/user_info.pkl',
         '/Users/zhanghaoyang04/Desktop/Movie_Recsys/cache/val_data.pkl',
-        type=type
+        type=type,
+        history_len=5,
+        kind_len=5,
     )
     if type == 'test':
         return DataLoader(dataset, batch_size=1, shuffle=False, num_workers=num_workers)
