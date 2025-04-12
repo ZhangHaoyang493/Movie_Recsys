c21e49a0600814d8ba5c8dc7e5e4447f4bc224f1
zhanghaoyang <841461378@qq.com>
2025-04-12 14:45:27 +0800

    添加了划分测试集训练集的机制，并且完善了特征工程，可以处理缺失值

--- featureEngineering/user_feature_engineering.py
+++ featureEngineering/user_feature_engineering.py
@@ -19,7 +19,7 @@ class UserFeatureEngineering:
         self.write_user_his_favourite_movie_kind()
     
     def write_rating_dict(self):
-        with open('../data/ratings.dat', 'r') as f:
+        with open('../data/train_ratings.dat', 'r') as f:
             for line in f:
                 line = line.strip()
                 userid, movieid, score, timestamp = line.split('::')
@@ -48,37 +48,49 @@ class UserFeatureEngineering:
     # 将用户历史的平均打分加入用户的特征中
     def write_user_avg_score(self):
         for userid in self.user_feature_dict:
-            user_scores = [i[1] for i in self.rating_dict[userid]]
-            avg_score = sum(user_scores) / len(user_scores)
-            self.user_feature_dict[userid].append(avg_score)
+            # 如果userid从没有出现在打分历史中
+            if userid in self.rating_dict:
+                user_scores = [i[1] for i in self.rating_dict[userid]]
+                avg_score = sum(user_scores) / len(user_scores)
+                self.user_feature_dict[userid].append(avg_score)
+            # 否则的话，认为这个用户的平均打分就是2.5
+            else:
+                self.user_feature_dict[userid].append(2.5)
     
     # 将用户历史的打分的方差加入用户的特征中
     def write_user_std_score(self):
         for userid in self.user_feature_dict:
-            user_scores = [i[1] for i in self.rating_dict[userid]]
-            avg_score = sum(user_scores) / len(user_scores)
-            user_scores = [i - avg_score for i in user_scores]
-            user_scores = [i**2 for i in user_scores]
-            std_score = (sum(user_scores) / len(user_scores)) ** 0.5
-            self.user_feature_dict[userid].append(std_score)
+            if userid in self.rating_dict:
+                user_scores = [i[1] for i in self.rating_dict[userid]]
+                avg_score = sum(user_scores) / len(user_scores)
+                user_scores = [i - avg_score for i in user_scores]
+                user_scores = [i**2 for i in user_scores]
+                std_score = (sum(user_scores) / len(user_scores)) ** 0.5
+                self.user_feature_dict[userid].append(std_score)
+            else:
+                self.user_feature_dict[userid].append(0.0)
 
     # 获取用户最喜欢的前5个
     def write_user_his_favourite_movie_kind(self):
         for userid in self.user_feature_dict:
-            kind_dict = {}
-            for rating_info in self.rating_dict[userid]:
-                movieid = rating_info[0]
-                kinds = self.movie_dict[movieid][-1]
-                for k in kinds:
-                    if k in kind_dict:
-                        kind_dict[k] += 1
-                    else:
-                        kind_dict[k] = 1
-            kind_arr = list(kind_dict.items())
-            kind_arr = sorted(kind_arr, key=lambda x: x[1], reverse=True)
-            kind_arr = kind_arr[:5]
-            kind_arr = [i[0] for i in kind_arr]
-            self.user_feature_dict[userid].append(kind_arr)
+            if userid in self.rating_dict:
+                kind_dict = {}
+                for rating_info in self.rating_dict[userid]:
+                    movieid = rating_info[0]
+                    kinds = self.movie_dict[movieid][-1]
+                    for k in kinds:
+                        if k in kind_dict:
+                            kind_dict[k] += 1
+                        else:
+                            kind_dict[k] = 1
+                kind_arr = list(kind_dict.items())
+                kind_arr = sorted(kind_arr, key=lambda x: x[1], reverse=True)
+                kind_arr = kind_arr[:5]
+                kind_arr = [i[0] for i in kind_arr]
+                self.user_feature_dict[userid].append(kind_arr)
+            else:
+                # 如果userid不曾出现在打分历史中，就把热度最高的三个类型给这个用户
+                self.user_feature_dict[userid].append(['Comedy', 'Drama', 'Action'])
     
     def save_user_feature_to_csv(self, save_path):
         with open(save_path, 'w') as f:
