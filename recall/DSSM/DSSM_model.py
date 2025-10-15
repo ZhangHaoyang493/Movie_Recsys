eac9a0beb6aac25c52f7523f50e0073e8e4b084b
Haoyang Zhang <841461378@qq.com>
2025-10-15 22:53:44 +0800

    完善DSSM，修复bug

--- recall/DSSM/DSSM_model.py
+++ recall/DSSM/DSSM_model.py
@@ -6,10 +6,14 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import faiss
+import numpy as np
+
+from DataReader.data_reader import DataReader
+from tqdm import tqdm
 
 # BaseModel继承于LightningModule
 class DSSM(BaseModel):
-    def __init__(self, config_path):
+    def __init__(self, config_path, dataloaders={}):
         super(DSSM, self).__init__(config_path)
         
         # 定义DSSM的网络结构
@@ -25,6 +29,11 @@ class DSSM(BaseModel):
             nn.ReLU(),
             nn.Linear(128, 16)
         )
+        
+        self.movies_dataloader = dataloaders.get('movies_dataloader', None)
+        self.val_dataloader_ = dataloaders.get('val_dataloader', None)
+
+       
 
     def forward(self, x):
         user_vector = self.get_user_embedding(x)  # 获取用户特征向量
@@ -61,21 +70,7 @@ class DSSM(BaseModel):
 
 
     def validation_step(self, batch, batch_idx):
-        user_vector = self.get_user_embedding(batch)  # 获取用户特征向量
-        item_vector = self.get_item_embedding(batch)  # 获取物品特征向量
-        
-        user_emb = self.user_fc(user_vector)  # 用户特征通过全连接层  bx16
-        item_emb = self.item_fc(item_vector)  # 物品特征通过全连接层  bx16
-
-        # 归一化
-        user_emb = F.normalize(user_emb, p=2, dim=1)
-        item_emb = F.normalize(item_emb, p=2, dim=1)
-
-        # 计算正样本的相似度
-        pos_scores = torch.sum(user_emb * item_emb, dim=1).mean()
-
-        self.log('val_pos_scores', pos_scores.item())
-        return pos_scores
+        pass
 
     def test_step(self, batch, batch_idx):
         pass
@@ -118,5 +113,54 @@ class DSSM(BaseModel):
         item_feature_vector = torch.cat(item_embeddings, dim=1)  # 在特征维度上拼接
         return item_feature_vector
     
-    def hit_rate(self):
-        pass
+    @torch.no_grad()
+    def hit_rate(self, k=10):
+        hits_num = 0
+        for batch in tqdm(self.val_dataloader_, desc="Evaluating Hit Rate", ncols=100):
+            for key in batch:
+                if isinstance(batch[key], torch.Tensor):
+                    batch[key] = batch[key].to(self.device)
+            user_vector = self.get_user_embedding(batch)  # 获取用户特征向量
+            user_emb = self.user_fc(user_vector)  # 用户特征通过全连接层  bx16
+            user_emb = F.normalize(user_emb, p=2, dim=1)
+            user_emb = user_emb.cpu().numpy()
+
+            D, I = self.index.search(user_emb, k)  # 搜索top-k的物品
+
+            # 将索引映射到item id
+            I = [[self.idx_item_emb_dic[idx] for idx in user_indices] for user_indices in I]
+
+            # 计算命中率
+            targets = batch[2].cpu().numpy()  # 假设item_id是物品的真实ID
+            hits = 0
+            for i in range(len(targets)):
+                if targets[i] in I[i]:
+                    hits_num += 1
+        hit_rate = hits_num / len(self.val_dataloader_.dataset)
+        self.log('val_hit_rate', hit_rate)
+        print(f"Hit Rate@{k}: {hit_rate}")
+
+    @torch.no_grad()
+    def on_train_epoch_end(self):
+        all_item_embeddings = []
+        self.idx_item_emb_dic = {}
+        idx = 0
+        for batch in tqdm(self.movies_dataloader, desc="Building Item Embeddings", ncols=100):
+            for key in batch:
+                if isinstance(batch[key], torch.Tensor):
+                    batch[key] = batch[key].to(self.device)
+            item_vector = self.get_item_embedding(batch)  # 获取物品特征向量
+            item_emb = self.item_fc(item_vector)  # 物品特征通过全连接层  bx16
+            item_emb = F.normalize(item_emb, p=2, dim=1)
+            item_emb = item_emb.cpu().numpy()
+            all_item_embeddings.append(item_emb)
+            for i, item_id in enumerate(batch[2].cpu().numpy()): # 2是item id的slot_id
+                self.idx_item_emb_dic[idx] = item_id  # 记录item_id对应的embedding在all_item_embeddings中的索引
+                idx += 1
+
+        self.all_item_embeddings = np.concatenate(all_item_embeddings, axis=0)
+        self.index = faiss.IndexFlatIP(self.all_item_embeddings.shape[1])  # 内积相似度
+        self.index.add(self.all_item_embeddings)  # 添加所有物品向量到索引
+
+        # 计算验证集的命中率
+        self.hit_rate(k=50)
